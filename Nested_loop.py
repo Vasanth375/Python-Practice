c11401d44b8b0509d6075487f37c4d1da3309bff
Vasanth375 <77767837+Vasanth375@users.noreply.github.com>
2021-03-03 23:37:37 +0530

    Create Nested_loop.py

---
+++ Nested_loop.py
@@ -0,0 +1,19 @@
+ave_names=[1,2,3,4,5]
+
+#first list declaration
+
+bb=[6,7,8,9,10]
+
+#second list declared
+
+for x in ave_names:
+
+	#1st loop(outter loop)	
+
+	for y in bb:
+
+		#2nd loop (outer loop)
+
+		
+
+		print('1st',x,'2nd', y)
