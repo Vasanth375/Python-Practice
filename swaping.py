d52f62e4ec19b671db58855a743cbe50a56490ca
Vasanth375 <77767837+Vasanth375@users.noreply.github.com>
2021-03-04 14:39:38 +0530

    Create swaping.py

---
+++ swaping.py
@@ -0,0 +1,23 @@
+"""python program to swap normal
+
+variables"""
+
+a,b=1,2
+
+print('values before swaping')
+
+print('a =',a,'b =',b)
+
+#swaping statements
+
+temp=a
+
+a=b
+
+b=temp
+
+#fetching result
+
+print('values after swaping')
+
+print('a =',a,'b =',b)
