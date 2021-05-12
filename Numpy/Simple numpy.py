9a56da0647c94c8c32917f4cf58837920c237da7
Vasanth <vasanthnimmala69@outlook.com>
2021-05-12 16:13:30 +0530

    Create Simple numpy.py

---
+++ Numpy/Simple numpy.py
@@ -0,0 +1,6 @@
+import numpy as np
+#importing numpy module
+arr = np.array([1, 2, 3, 4, 5])
+#np is instance for numpy
+print(arr)#displays array
+print(np.__version__)#displays current version of numpy
